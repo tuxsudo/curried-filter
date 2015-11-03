4a9ffc1cca7f27a8f05cb4bd4be5511bdef28370
Jared Anderson <jared@tuxsudo.com>
2015-11-03 11:43:33 -0700

    updated curry module cuz bugs

--- test/test.js
+++ test/test.js
@@ -1,7 +1,19 @@
 import test from 'tape';
 import filter from '../index';
 
-test('Curry Filter', t=> {
+
+test('filters lazily (curried)', t=> {
+
+	t.plan( 2 );
+
+	t.deepEqual([6,7], filter( n=>n>5, [2,3,5,6,7]) );
+	t.deepEqual([10,15,20], filter( n=>n>5, [5, 10, 15, 20]) );
+
+});
+
+
+
+test('filters lazily (curried)', t=> {
 	t.plan(2);
 
 	let gt5 = filter(n=>n>5);
